bbdd0fb5dd662b26f2b685f71b3d748d50213097
Jacob Canedy <89418437+JacobACan@users.noreply.github.com>
2025-12-15 16:00:02 -0500

    day 1

---
+++ 2025/01-25/01-25/Program.cs
@@ -0,0 +1,42 @@
+﻿const int min = 0;
+const int max = 99;
+int answer = 0;
+const int startPos = 50;
+
+void solve()
+{
+    var inputStream = new StreamReader("C:\\Jacob\\Code\\advent-of-code\\2025\\01-25\\01-25\\input.txt");
+    var line = inputStream.ReadLine();
+
+    int pos = startPos;
+    while (line != null)
+    {
+        char direction = line[0];
+        for (int i = 0; i < Int32.Parse(line.Substring(1, line.Length - 1)); i++)
+        {
+            if (direction == 'L')
+            {
+                pos -= 1;
+                if (pos < min)
+                {
+                    pos = max;
+                }
+            }
+            else if (direction == 'R')
+            {
+                pos += 1;
+                if (pos > max)
+                {
+                    pos = min;
+                }
+            }
+            if (pos == 0)
+            {
+                answer += 1;
+            }
+        }
+        line = inputStream.ReadLine();
+    }
+    Console.WriteLine(answer);
+}
+solve();
