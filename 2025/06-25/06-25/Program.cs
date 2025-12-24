e2e2e764498a4ee95b102a3e17c23243bf4364d1
Jacob Canedy <89418437+JacobACan@users.noreply.github.com>
2025-12-24 16:47:50 -0500

    6 & 7

---
+++ 2025/06-25/06-25/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Diagnostics;
+
+var reader = new StreamReader("C:\\Jacob\\Code\\advent-of-code\\2025\\06-25\\input.txt");
+var line = reader.ReadLine();
+List<string[]> rows1 = [];
+List<string> rows2 = [];
+
+while (line != null)
+{
+    //Part 1
+    rows1.Add(line.Split().Where(s => !String.IsNullOrWhiteSpace(s)).ToArray());
+    rows2.Add(line);
+
+    line = reader.ReadLine();
+}
+
+//Part 1
+string[] operations = rows1[rows1.Count - 1];
+for (int r = 1; r < rows1.Count - 1; r++)
+{
+    var row = rows1[r];
+    var prevRow = rows1[r - 1];
+    for (int c = 0; c < rows1[0].Length; c++)
+    {
+        var operation = operations[c];
+        var col = row[c];
+        var prevCol = prevRow[c];
+        var colVal = long.Parse(col);
+        var prevColVal = long.Parse(prevCol);
+
+        if (operation == "*")
+        {
+            rows1[r][c] = (colVal * prevColVal).ToString();
+        }
+        else if (operation == "+")
+        {
+            rows1[r][c] = (colVal + prevColVal).ToString();
+        }
+    }
+}
+
+//Part 2
+long total = 0;
+int operationIndex = 0;
+long colTotal = 0;
+bool newCol = true;
+for (int c = 0; c < rows2.First().Length; c++)
+{
+    string innerColTotal = "";
+
+    for (int r = 0; r < rows2.Count - 1; r++)
+    {
+        innerColTotal += rows2[r][c];
+    }
+
+    if (string.IsNullOrWhiteSpace(innerColTotal))
+    {
+        total += colTotal;
+        colTotal = 0;
+        operationIndex++;
+        newCol = true;
+    }
+    else
+    {
+
+        switch (operations[operationIndex])
+        {
+            case "*":
+                if (newCol)
+                {
+                    colTotal = long.Parse(innerColTotal);
+                }
+                else
+                {
+
+                    colTotal *= long.Parse(innerColTotal);
+                }
+                break;
+            case "+":
+                colTotal += long.Parse(innerColTotal);
+                break;
+        }
+        newCol = false;
+    }
+
+    if (c == rows2.First().Length - 1)
+    {
+        total += colTotal;
+    }
+}
+
+
+
+Console.WriteLine(rows1[rows1.Count - 2].Aggregate((a, b) => (long.Parse(a) + long.Parse(b)).ToString()));
+Console.WriteLine();
+Console.WriteLine(total);
---
+++ 2025/07-25/07-25/Program.cs
@@ -0,0 +1,105 @@
+﻿var file = new StreamReader("C:\\Jacob\\Code\\advent-of-code\\2025\\07-25\\input.txt");
+string? line = file.ReadLine();
+
+List<string> map = [];
+while (line != null)
+{
+    map.Add(line);
+    line = file.ReadLine();
+}
+
+List<long[]> map2 = new List<long[]>(map.Select(l =>
+{
+    var line = new long[l.Length];
+    for (int i = 0; i < l.Length; i++)
+    {
+        if (l[i] == '^')
+        {
+
+            line[i] = -1;
+        }
+        else if (l[i] == 'S')
+        {
+            line[i] = 1;
+
+        }
+        else
+        {
+            line[i] = 0;
+        }
+    }
+    return line;
+}));
+
+//Part 1
+int ans = 0;
+for (int i = 1; i < map.Count; i++)
+{
+    var chars = map[i].ToArray();
+
+    var prevChars = map[i - 1].ToArray();
+    for (int j = 0; j < chars.Length; j++)
+    {
+        bool foundSplitter = chars[j] == '^';
+        bool foundBeam = prevChars[j] == '|' || prevChars[j] == 'S';
+        if (foundSplitter && foundBeam)
+        {
+            ans += 1;
+            chars[j - 1] = '|';
+            chars[j + 1] = '|';
+
+        }
+        else if (foundBeam)
+        {
+            chars[j] = '|';
+        }
+
+    }
+    map[i] = new string(chars);
+    Console.WriteLine(map[i]);
+}
+
+Console.WriteLine(ans);
+
+for (int r = 1; r < map2.Count; r++)
+{
+    var row = map2[r];
+    var prevRow = map2[r - 1];
+    for (int c = 0; c < row.Length; c++)
+    {
+        var val = row[c];
+
+        if (row[c] != -1)
+        {
+            if (c + 1 < row.Length && row[c + 1] == -1 && prevRow[c + 1] != -1)
+                val += prevRow[c + 1];
+
+            if (c - 1 > 0 && row[c - 1] == -1 && prevRow[c - 1] != -1)
+                val += prevRow[c - 1];
+
+            if (prevRow[c] != -1)
+                val += prevRow[c];
+
+        }
+
+        map2[r][c] = val;
+    }
+}
+
+long ans2 = 0;
+foreach (var item in map2[map2.Count - 1])
+{
+    ans2 += item;
+}
+
+//map2.ForEach(r =>
+//{
+//    Console.WriteLine(); foreach (var item in r)
+//    {
+//        Console.Write(item);
+//    }
+//});
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine(ans2);
